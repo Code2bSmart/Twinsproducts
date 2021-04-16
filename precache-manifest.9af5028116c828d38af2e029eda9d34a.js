7ac14bb8ea312cdb207ddc99d839076970139a76
Code2bSmart <68510698+Code2bSmart@users.noreply.github.com>
2021-04-16 16:00:26 +0530

    Add files via upload

---
+++ precache-manifest.9af5028116c828d38af2e029eda9d34a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15331bacbd6bdb50a2809eb41564f422",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6259efbd142fd2478faa",
+    "url": "/static/css/main.40ac0f07.chunk.css"
+  },
+  {
+    "revision": "38e6e295405c1c8d5df2",
+    "url": "/static/js/2.25f416f2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.25f416f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6259efbd142fd2478faa",
+    "url": "/static/js/main.95d11909.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "8bf1af37dd02198b256725e83a203795",
+    "url": "/static/media/cart.8bf1af37.svg"
+  },
+  {
+    "revision": "2c5552a10c3ab0a115c9484f908f982d",
+    "url": "/static/media/close.2c5552a1.svg"
+  },
+  {
+    "revision": "748792142793bd6eaa15bbb28b7d6f55",
+    "url": "/static/media/menu.74879214.svg"
+  }
+]);
